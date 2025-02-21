daed9389af667ef1c80151e6435fb16e466018bb
mamund <mamund@yahoo.com>
2025-02-21 13:10:56 -0500

    upeate rounding

--- scripts/outsidely-helper.js
+++ scripts/outsidely-helper.js
@@ -60,7 +60,6 @@ function metersToFeet(meters) {
 function metersToMiles(meters) {
   var rtn = "";
   if(meters) {
-    //rtn = Math.ceil(meters*0.000621371192).toString()+ " miles";
     rtn = meters*0.000621371192;
     rtn = rtn*10;
     rtn = Math.round(rtn);
