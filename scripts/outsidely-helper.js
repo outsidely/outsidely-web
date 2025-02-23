1aac8a7eb4fdf3c0d08ba8e08047ae127ee87255
mamund <mamund@yahoo.com>
2025-02-23 15:01:51 -0500

    tweak elapsed time display

--- scripts/outsidely-helper.js
+++ scripts/outsidely-helper.js
@@ -14,7 +14,7 @@ function formatTime(seconds) {
       const minutes = Math.floor((seconds % 3600) / 60);
       const remainingSeconds = seconds % 60;
     
-      const formattedHours = String(hours).padStart(2, '0');
+      const formattedHours = String(hours).padStart(2, ' ');
       const formattedMinutes = String(minutes).padStart(2, '0');
       const formattedSeconds = String(remainingSeconds).padStart(2, '0');
     
